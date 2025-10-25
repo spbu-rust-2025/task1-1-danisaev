d6ec04e152e51e43ee077b65ea652c9899d95ee0
Daniil Isaev <isaev.daniil06@gmail.com>
2025-10-25 14:49:04 +0300

    Add the solution of the first task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut input_string = String::new();
+    io::stdin().read_line(&mut input_string).unwrap();
+    let mut splited_string = input_string.split_whitespace();
+    let first_number: u32 = splited_string.next().unwrap().parse().unwrap();
+    let second_number: u32 = splited_string.next().unwrap().parse().unwrap();
+    println!("{}", first_number + second_number);
 }
